6adb38b8d4c33233aa948e45cefb1437994ec690
unknown <sazwerling@gmail.com>
2023-03-07 08:33:52 -0800

    create redux store and wrap App w Provider comp

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = createRoot(el);
 
+root.render(
+   <Provider store={store}>
+      <App />
+   </Provider>
+);
 
-root.render(<App />);
-
-
-
-// State: car name(string input), car cost(number input), 
-// search term(string input), 
+// State: car name(string input), car cost(number input),
+// search term(string input),
 // Cars(array of objects{id: , name: , cost: })
-// DERIVED STATE -- total cost(number), Match between input and car already in state 
+// DERIVED STATE -- total cost(number), Match between input and car already in state
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { carReducer, changeSearchTerm, addCar, removeCar } from "./slices/carSlice";
+import { formReducer, changeCost, changeName } from "./slices/formSlice";
+
+
+
+const store = configureStore({
+    reducer: {
+        cars: carReducer,
+        form: formReducer
+    }
+});
+
+
+export { store, changeCost, changeName, changeSearchTerm, addCar, removeCar };
