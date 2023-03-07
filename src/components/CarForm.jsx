9bdac28421be8214d296a5287a4b016ff670132c
unknown <sazwerling@gmail.com>
2023-03-07 09:49:51 -0800

    create and connect submit on car form

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm(){
     const dispatch = useDispatch();
     const {name, cost} = useSelector((state) => {
+        console.log(state)
         return {
             name: state.form.name,
             cost: state.form.cost
@@ -17,11 +18,16 @@ function CarForm(){
         const carCost = parseFloat(e.target.value) || 0;
         dispatch(changeCost(carCost))
     }
+    const handleSubmit = (e) => {
+        console.log(e)
+        e.preventDefault()
+        dispatch(addCar({name, cost}))
+    }
 
     return(
         <div className="car-form panel">
             <h3 className="subtitle is-3">add car</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="field-group">
                     <div className="field">
                         <label className="label">name</label>
@@ -31,6 +37,9 @@ function CarForm(){
                         <label className="label">cost</label>
                         <input className="input is-expanded" value={cost || ''} onChange={handleCostChange} type="number" />
                     </div>
+                </div>
+                <div className="field">
+                    <button className="button is-link">submit</button>
                 </div>
             </form>
         </div>
