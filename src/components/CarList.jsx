acb9e5d0b492ce5831c3a9e748543d52d7c8f9dd
unknown <sazwerling@gmail.com>
2023-03-07 14:54:20 -0800

    implement search filter

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -2,14 +2,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 
 function CarList() {
-    const dispatch = useDispatch()
-   const cars = useSelector((state) => {
-      return state.cars.list;
+   const dispatch = useDispatch();
+
+   const cars = useSelector(({ cars: {list, searchTerm}}) => {
+    //filtering logic
+      return list.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()))
    });
 
    const handleCarDelete = (car) => {
-    dispatch(removeCar(car.id))
-   }
+      dispatch(removeCar(car.id));
+   };
 
    const renderedList = cars.map((car) => {
       return (
@@ -17,12 +19,22 @@ function CarList() {
             <p>
                {car.name} - ${car.cost}
             </p>
-            <button onClick={() => handleCarDelete(car)} className="button is-danger">delete</button>
+            <button
+               onClick={() => handleCarDelete(car)}
+               className="button is-danger"
+            >
+               delete
+            </button>
          </div>
       );
    });
 
-   return <div className="car-list">{renderedList}<hr /></div>;
+   return (
+      <div className="car-list">
+         {renderedList}
+         <hr />
+      </div>
+   );
 }
 
 export default CarList;
