4e16f43fbbc157aa9770d4e7330c28818f4eee30
unknown <sazwerling@gmail.com>
2023-03-07 14:39:15 -0800

    search input

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,7 +1,29 @@
-function CarSearch(){
-    return(
-        <div>car search</div>
-    )
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
+
+function CarSearch() {
+    const dispatch = useDispatch()
+    const handleSearchTermChange = (e) => {
+        dispatch(changeSearchTerm(e.target.value))
+    }
+    const searchTerm = useSelector((state) => {
+        return state.cars.searchTerm
+    })
+   
+   return (
+      <div className="list-header">
+         <h3 className="title is-3">My Cars</h3>
+         <div className="search field is-horizonatl">
+            <label className="label">Search</label>
+            <input
+               onChange={handleSearchTermChange}
+               className="input"
+               value={searchTerm}
+               type="text"
+            />
+         </div>
+      </div>
+   );
 }
 
 export default CarSearch;
